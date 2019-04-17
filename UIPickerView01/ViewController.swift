e9e69aa625cc7483492f97fd835b908baa354601
D7703_07 <d7703_07@lulzsec.local>
2019-04-17 16:45:51 +0900

    ,,

--- UIPickerView01/ViewController.swift
+++ UIPickerView01/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var outLabel: UILabel!
+    @IBOutlet weak var myPickerView: UIPickerView!
+    var color = ["RED", "GREEN", "BLUE", "WHITE", "YELLOW"]
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        myPickerView.delegate = self
+        myPickerView.dataSource = self
+      
     }
-
-
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return color.count
+    }
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return color[row]
+    }
+    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            print(color[row])
+        
+        outLabel.text = color[row]
+        }
+    public func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 100.0
+    }
 }
+
 
